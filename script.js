fbde1f82b360889fe50ea18f258815ac597b953a
[Angelo Kolios] <49371480+koliosor@users.noreply.github.com>
2023-02-27 13:15:37 +0200

    Made the code more readable, added Jquery and bootstrap.

--- script.js
+++ script.js
@@ -1,167 +1,93 @@
-let playerScore = 0;
-let computerScore = 0;
-let tieScore = 0;
-let gameFinished = false;
 const livefeed = document.querySelector(".feed");
+/// STATE VARIABLES ///
+var game_in_progress = false;
+var playerSelection;
+var computerChoice;
+let amountRounds;
+let roundWonBy;
+let score = { 'playerScore': 0, 'tieScore': 0, 'computerScore': 0 }
 
-const buttons = document.querySelectorAll(".wrap button");
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissors = document.querySelector(".scissors");
-const start = document.querySelector("#start");
-const reset = document.querySelector("#reset");
-
-const getComputersChoice = () => {
-  let rand = Math.floor(Math.random() * 3);
-  let computersChoice;
-  if (rand === 0) {
-    computersChoice = "rock";
-  } else if (rand === 1) {
-    computersChoice = "scissors";
-  } else {
-    computersChoice = "paper";
-  }
-  return computersChoice;
+/// CONSTANTS ///
+let game_win = {
+  rock:     { paper: 1, rock: 0, scissors: 2 },
+  paper:    { paper: 0, rock: 2, scissors: 1 },
+  scissors: { paper: 2, rock: 1, scissors: 0 },
+};
+let choicesIcons = {
+  rock: "✊",
+  paper: "✋",
+  scissors: "✌",
 };
 
-const playRound = (computersChoice, playerSelection) => {
-  if (playerSelection === "rock") {
-    if (computersChoice === "rock") {
-      ++tieScore;
 
-      displayMessage(
-        `Player selected ${playerSelection} and computer ${computersChoice} its a tie.`
-      );
-    } else if (computersChoice === "scissors") {
-      ++playerScore;
-      displayMessage(
-        `Player selected ${playerSelection}. Computer chose ${computersChoice}. Player wins the round.`
-      );
-    } else if (computersChoice === "paper") {
-      ++computerScore;
-      displayMessage(
-        `Player selected ${playerSelection}. Computer chose ${computersChoice}. Computer wins the round.`
-      );
-    }
-  } else if (playerSelection === "scissors") {
-    if (computersChoice === "scissors") {
-      tieScore++;
-      displayMessage(
-        `Player selected ${playerSelection} and computer ${computersChoice} its a tie.`
-      );
-    } else if (computersChoice === "rock") {
-      ++computerScore;
-      displayMessage(
-        `Player selected ${playerSelection}. Computer chose ${computersChoice}. Computer wins the round.`
-      );
-    } else if (computersChoice === "paper") {
-      ++playerScore;
-      displayMessage(
-        `Player selected ${playerSelection}. Computer chose ${computersChoice}. Player wins the round.`
-      );
-    }
-  } else {
-    if (computersChoice === "paper") {
-      tieScore++;
-      displayMessage(
-        `Player selected ${playerSelection} and computer ${computersChoice} its a tie.`
-      );
-    } else if (computersChoice === "rock") {
-      ++playerScore;
-      displayMessage(
-        `Player selected ${playerSelection}. Computer chose ${computersChoice} Player wins the round`
-      );
-    } else if (computersChoice === "scissors") {
-      ++computerScore;
-      displayMessage(
-        `Player selected ${playerSelection}. Computer chose ${computersChoice}. Computer wins the round`
-      );
-    }
-  }
-};
+/// ######################################## ///
+///           LOGIC FUNCTIONS                ///
+/// ######################################## ///
+$('#start').click( function (e) {
+	amountRounds = $("#quantity").val();
+	if (amountRounds == '') {
+		alert('Enter number of rounds');
+		return
+	} else {
+		amountRounds = parseInt($("#quantity").val());
+		$('#quantity').addClass('d-none');
+		$('#restart').removeClass('d-none');
+		$(this).addClass('d-none');
+		game_in_progress = true;
+	}
+})
 
-start.addEventListener("click", startGame);
-function startGame() {
-  if (isGameOver()) {
-    return;
-  }
-  if (livefeed.childNodes.length == 1) {
-    livefeed.removeChild(livefeed.lastChild);
-  }
-  paper.addEventListener("click", handleclick);
-  rock.addEventListener("click", handleclick);
-  scissors.addEventListener("click", handleclick);
-  reset.addEventListener("click", resetGame);
-}
+$('#restart').click(function (e) {
+	game_in_progress = true
+	score.playerScore = 0;
+	score.computerScore = 0;
+	score.tieScore = 0;
+	updateDisplayScore();
+	livefeed.innerHTML = "";
+});
 
-function handleclick(e) {
-  const choice = e.target.classList[0];
-  // console.log(e.target.textContent);
-  if (isGameOver()) {
-    displayResult();
-    resetGame();
-    clearList();
-  }
-  playRound(getComputersChoice(), choice);
-}
+// PLAY ROUND FUNCTION
+$('.sign').click( function (e) {
+	// if game not in progress return
+	if (!game_in_progress) { return }
 
-function isGameOver() {
-  return playerScore === 4 || computerScore === 4;
-}
+	// get choices
+	playerSelection = $(this).attr('id');
+	computerChoice = ['rock', 'scissors', 'paper'][ Math.floor(Math.random() * 3) ];
 
-// buttons.forEach((button) => {
-//    button.addEventListener("click", handleclick);
-//  });
+	// game over condition
+	if (score['playerScore'] >= amountRounds || score['computerScore'] >= amountRounds) {
+		displayResult();
+		game_in_progress = false
+	} else {
+		// game isn't over
+		// update score
+		roundWonBy = game_win[playerSelection][computerChoice]; // 0 or 1 or 2
+		score[ ['tieScore', 'computerScore', 'playerScore'][roundWonBy] ] ++
+		updateDisplayScore();
+	}
+})
+/// ######################################## ///
 
-function displayMessage(str) {
-  livefeed.animate([{ opacity: 0 }, { opacity: 1 }], {
-    duration: 300,
-    fill: "forwards",
-    iterations: 1,
-    delay: 1,
-    easing: "ease-out",
-  });
-  const paragraph = createParagraph();
-  paragraph.textContent = str;
-
-  livefeed.appendChild(paragraph);
-}
-
-function createParagraph() {
-  const par = document.createElement("p");
-
-  return par;
-}
-
-function createHeading() {
-  const heading = document.createElement("h1");
-  return heading;
-}
-
-function clearList() {
-  // console.log(livefeed.children);
-  let paragraph = livefeed.querySelectorAll("p");
-  for (let i = 0; i <= paragraph.length; i++) {
-    livefeed.removeChild(paragraph[i]);
-  }
-}
-
-function resetGame() {
-  playerScore = 0;
-  computerScore = 0;
-  tieScore = 0;
-  paper.removeEventListener("click", handleclick);
-  rock.removeEventListener("click", handleclick);
-  scissors.removeEventListener("click", handleclick);
+/// ######################################## ///
+///           DISPLAY  FUNCTIONS             ///
+/// ######################################## ///
+function updateDisplayScore() {
+	$(".player-score").text(`Player Score:  ${score.playerScore}`);
+	$(".computer-score").text(`Computer Score: ${score.computerScore}`);
+	$(".tie-score").text(`Tie Score: ${score.tieScore}`);
 }
 
 function displayResult() {
-  const heading = createHeading();
-
-  if (playerScore > computerScore) {
-    heading.textContent = "Player wins the  Game.";
-  } else if (computerScore > playerScore) {
-    heading.textContent = "Computer wins the Game.";
-  }
-  livefeed.appendChild(heading);
+	const heading = document.createElement("h1");
+	if (score.playerScore > score.computerScore) {
+		heading.textContent = "Player wins the  Game.";
+	} else if (score.computerScore > score.playerScore) {
+		heading.textContent = "Computer wins the Game.";
+	}
+	livefeed.appendChild(heading);
 }
+
+/// ########################################## ///
+
+
